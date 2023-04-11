0810b761459136cb5103500323c2b18a0b1cc9ee
tanhauhau <lhtan93@gmail.com>
2023-04-11 23:56:21 +0800

    feat: add carousel component

---
+++ Chapter11/01-carousel/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Carousel from './Carousel.svelte';
+  let items = [];
+</script>
+
+<Carousel {items} let:currentItem let:next let:prev>
+  <button on:click={prev}>{'<'}</button>
+  <img src={currentItem} />
+  <button on:click={next}>{'>'}</button>
+</Carousel>
+
+<Carousel {items} let:currentItem let:setCurrentIndex>
+  <img src={currentItem} />
+  {#each items as _, index}
+    <button on:click={() => setCurrentIndex(index)}>
+      {index}
+    </button>
+  {/each}
+</Carousel>
---
+++ Chapter11/01-carousel/Carousel.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let items;
+  let currentIndex = 0;
+  $: currentItem = items[currentIndex];
+  const setCurrentIndex = (value) => { currentIndex = value; };
+  const prev = () => setCurrentIndex((currentIndex - 1 + items.length) % items.length);
+  const next = () => setCurrentIndex((currentIndex + 1) % items.length);
+</script>
+
+<slot {currentIndex} {currentItem} {setCurrentIndex} {prev} {next} />
