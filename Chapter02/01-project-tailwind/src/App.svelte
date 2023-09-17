88a0a990a4e9a68b807ac9427d5cdc838210af9e
tanhauhau <lhtan93@gmail.com>
2023-09-17 23:32:53 +0800

    feat: chapter 2

---
+++ Chapter02/01-project-tailwind/src/App.svelte
@@ -0,0 +1,3 @@
+<main>
+  <h1 class="text-center text-sky-400">Hello World</h1>
+</main>
---
+++ Chapter02/01-project-tailwind/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    // all files ends with .svelte in the src folder
+    './src/**/*.svelte',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
